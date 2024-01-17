eda0b9e14d38181e87c0db6b4cf5a8e560cb1ea1
wezer-pixel <jerryjuma104@gmail.com>
2024-01-17 14:28:36 +0300

    [TASK] Search from database

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+--- create a table users with id email, name
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
+
---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+--- ranks country origins of brands
+--- ordered by non-unique fans
+SELECT origin, SUM(fans) AS nb_fans
+	FROM metal_bands
+	GROUP BY origin
+	ORDER BY nb_fans DESC;
+
